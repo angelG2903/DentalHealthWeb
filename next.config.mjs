b9f518909d3c0607130d4bac9e1c7d454a8c935a
angelG2903 <angel.gabriel-1999@hotmail.com>
2024-12-07 21:18:30 -0600

    Creo que ya quedo x4

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         protocol: process.env.NEXT_PUBLIC_PROTOCOL,
         hostname: process.env.NEXT_PUBLIC_HOST_NAME,
-        port: process.env.NEXT_PUBLIC_PORT,
+        port: process.env.NEXT_PUBLIC_PORT || "",
         pathname: '/infrastructure/uploads/**',
       },
     ],
